36ca50e6a0de324da249173b1c27ee8b4e3624aa
Cristian Encalada <77868316+cristian-encalada@users.noreply.github.com>
2024-01-31 21:40:14 -0300

    Add function playRound. Fixed player selection vs computer

--- game.js
+++ game.js
@@ -1,8 +1,42 @@
 function getComputerChoice()
 {
     let options = ['Rock', 'Paper', 'Scissors']
-    let option = options[Math.floor(Math.random()*options.length)];
-    console.log(option);
+    let randomChoice = options[Math.floor(Math.random() * options.length)];
+    return randomChoice;
 }
 
-getComputerChoice();
+const playerSelection = "Rock";
+console.log(`Your selection: ${playerSelection}`);
+const computerSelection = getComputerChoice();
+console.log(`Computer selection ${computerSelection}`);
+
+function playRound(playerSelection, computerSelection)
+{
+    if (playerSelection == computerSelection)
+    {
+        return ('Tie!')
+    }
+    else if (playerSelection == 'Rock')
+    {
+        if (computerSelection == 'Scissors')
+            return (`You win! ${playerSelection} beats ${computerSelection}`);
+        else
+            return (`You lose! ${computerSelection} beats ${playerSelection}`);
+    }
+    else if (playerSelection == 'Paper')
+    {
+        if (computerSelection == 'Rock')
+            return (`You win! ${playerSelection} beats ${computerSelection}`);
+        else
+            return (`You lose! ${computerSelection} beats ${playerSelection}`);
+    }
+    else if (playerSelection == 'Scissors')
+    {
+        if (computerSelection == 'Paper')
+            return (`You win! ${playerSelection} beats ${computerSelection}`);
+        else
+            return (`You lose! ${computerSelection} beats ${playerSelection}`);
+    }
+}
+
+console.log(playRound(playerSelection, computerSelection));
